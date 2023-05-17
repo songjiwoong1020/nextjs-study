64bfc7fcc3b5017b71a369484a4d53ede9952800
songjiwoong <songjiwoong1020@gmail.com>
2023-05-17 20:22:20 +0900

    learning server side rendering

--- pages/index.js
+++ pages/index.js
@@ -3,22 +3,22 @@ import Head from "next/head"
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
-export default function Home () {
-  const [movies, setMovies] = useState();
-  useEffect(() => {
-    (async () => {
-      const {results} = await (
-        await fetch('/api/movies')
-      ).json();
-      setMovies(results);
-    })();
-  }, []);
+export default function Home ({ results }) {
+  // const [movies, setMovies] = useState();
+  // useEffect(() => {
+  //   (async () => {
+  //     const {results} = await (
+  //       await fetch('/api/movies')
+  //     ).json();
+  //     setMovies(results);
+  //   })();
+  // }, []);
 
   return (
     <div className="container">
       <Seo title='Home'/>
-      {!movies && <h4>Load.ing...</h4>}
-      {movies?.map(movie => (
+      {/* {!movies && <h4>Load.ing...</h4>} */}
+      {results?.map(movie => (
         <div className="movie" key={movie.id}>
            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
@@ -53,4 +53,13 @@ export default function Home () {
       `}</style>
     </div>
   )
+}
+
+export async function getServerSideProps() {
+  const { results } = await ( await fetch('http://localhost:3000/api/movies')).json();
+  return {
+    props: {
+      results
+    }
+  }
 }
